443fa09d673642c3abbd1e3443fbe636a7e5e4f9
rebcaborges <rborges.qa@gmail.com>
2025-06-16 11:57:20 -0300

    testes negativos

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -2,15 +2,45 @@
 
 describe('Funcionalidade login' , () => {
 
+        beforeEach(()=> {
+                cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+
+        });
+
+
+        afterEach(() => {
+                cy.screenshot()
+                
+        });
+
+
+
+
     it ('Deve fazer login  com sucesso', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        
         cy.get('#username').type('rborges.qa@gmail.com.br')
         cy.get('#password').type('Borges00@@')
         cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)') .should('be.visible').and('contain', 'Olá, rborges.qa-5403');
+    
+        })
 
 
-cy.get('.woocommerce-MyAccount-content > :nth-child(2)')
-  .should('be.visible')
-  .and('contain', 'Olá, rborges.qa-5403');
-    })
-})
+        it('Deve exibir uma mensagem de erro ao inserir usuário invalido', () => {
+                cy.get('#username').type('rbrges@gmail.com.br')
+                cy.get('#password').type('Borges00@@')
+                cy.get('.woocommerce-form > .button').click()
+                cy.get('.woocommerce-error > li').should('contain','Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+                cy.get('.woocommerce-error').should('exist')
+        });
+
+
+        it('Deve exibir uma mensagem de erro ao inserir senha invalida',() => {
+                cy.get('#username').type('rbrges@gmail.com.br')
+                cy.get('#password').type('Borges00@@1')
+                cy.get('.woocommerce-form > .button').click()
+                cy.get('.woocommerce-error > li').should('contain','Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+                cy.get('.woocommerce-error').should('exist')
+        });
+                
+        })
