67eeb395e584a87cd60d5ef2357e709a0e420926
rebcaborges <rborges.qa@gmail.com>
2025-06-23 16:15:42 -0300

    correções

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -19,7 +19,7 @@ describe('Funcionalidade login' , () => {
     it ('Deve fazer login  com sucesso', () => {
         
         cy.get('#username').type('rborges.qa@gmail.com.br')
-        cy.get('#password').type('Borges00@@')
+        cy.get('#password').type('Teste00@@')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)') .should('be.visible').and('contain', 'Olá, rborges.qa-5403');
     
@@ -28,7 +28,7 @@ describe('Funcionalidade login' , () => {
 
         it('Deve exibir uma mensagem de erro ao inserir usuário invalido', () => {
                 cy.get('#username').type('rbrges@gmail.com.br')
-                cy.get('#password').type('Borges00@@')
+                cy.get('#password').type('Teste00@@')
                 cy.get('.woocommerce-form > .button').click()
                 cy.get('.woocommerce-error > li').should('contain','Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
                 cy.get('.woocommerce-error').should('exist')
@@ -37,7 +37,7 @@ describe('Funcionalidade login' , () => {
 
         it('Deve exibir uma mensagem de erro ao inserir senha invalida',() => {
                 cy.get('#username').type('rbrges@gmail.com.br')
-                cy.get('#password').type('Borges00@@1')
+                cy.get('#password').type('Teste00@@1')
                 cy.get('.woocommerce-form > .button').click()
                 cy.get('.woocommerce-error > li').should('contain','Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
                 cy.get('.woocommerce-error').should('exist')
