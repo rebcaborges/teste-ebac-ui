bfdad203a0e1c0705e3dd5ba480d30e193e90b49
Rebeca Borges <87546461+rebcaborges@users.noreply.github.com>
2025-06-26 13:39:57 -0300

    Update login.cy.js

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,11 +1,9 @@
 /// <reference types="cypress"/>
 
-<<<<<<< HEAD
 //const { it } = require('@faker-js/faker');
 const perfil  = require ('../../fixtures/perfil.json')
 
-=======
->>>>>>> e2279506f63f3a169cfd90900f1d92492186afa4
+
 describe('Funcionalidade login' , () => {
 
         beforeEach(()=> {
@@ -13,8 +11,7 @@ describe('Funcionalidade login' , () => {
 
         });
 
-<<<<<<< HEAD
-        //afterEach(() => {
+       //afterEach(() => {
         //        cy.screenshot()        
        // });
 
@@ -29,12 +26,10 @@ describe('Funcionalidade login' , () => {
         it('Deve exibir uma mensagem de erro ao inserir usuário invalido', () => {
                 cy.get('#username').type('rbrges@gmail.com.br')
                 cy.get('#password').type('Teste00@@##')
-=======
 
-        afterEach(() => {
-                cy.screenshot()
-                
-        });
+     //   afterEach(() => {
+     //           cy.screenshot()
+    //           });
 
 
 
@@ -52,26 +47,21 @@ describe('Funcionalidade login' , () => {
         it('Deve exibir uma mensagem de erro ao inserir usuário invalido', () => {
                 cy.get('#username').type('rbrges@gmail.com.br')
                 cy.get('#password').type('Teste00@@')
->>>>>>> e2279506f63f3a169cfd90900f1d92492186afa4
                 cy.get('.woocommerce-form > .button').click()
                 cy.get('.woocommerce-error > li').should('contain','Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
                 cy.get('.woocommerce-error').should('exist')
         });
 
-<<<<<<< HEAD
         it('Deve exibir uma mensagem de erro ao inserir senha invalida',() => {
                 cy.get('#username').type('rbrges.qaa@gmail.com.br')
-=======
 
         it('Deve exibir uma mensagem de erro ao inserir senha invalida',() => {
                 cy.get('#username').type('rbrges@gmail.com.br')
->>>>>>> e2279506f63f3a169cfd90900f1d92492186afa4
                 cy.get('#password').type('Teste00@@1')
                 cy.get('.woocommerce-form > .button').click()
                 cy.get('.woocommerce-error > li').should('contain','Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
                 cy.get('.woocommerce-error').should('exist')
         });
-<<<<<<< HEAD
 
                 it('Deve fazer logion com sucesso -  usando massa de dados', () => {
                 cy.get('#username').type(perfil.usuario)
@@ -97,9 +87,6 @@ describe('Funcionalidade login' , () => {
                         cy.get('.woocommerce-MyAccount-content > :nth-child(2)')
                 
          });
-        
-      
-=======
+             
                 
->>>>>>> e2279506f63f3a169cfd90900f1d92492186afa4
 })
